1937c51fb1c4bdd16627a12bbda8b84505705e45
Aleksandar Todorov <42151221+aleksandar-todorov@users.noreply.github.com>
2020-01-12 16:50:16 +0200

    Updates

---
+++ precache-manifest.846170ebc20a86145adb054bc6424427.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc85c4120da68aff2575134434c3e85b",
+    "url": "/Quiz-Shop/index.html"
+  },
+  {
+    "revision": "ab02da4db701cc46c55f",
+    "url": "/Quiz-Shop/static/css/main.eeb29831.chunk.css"
+  },
+  {
+    "revision": "531e494ee8b5786f1b8d",
+    "url": "/Quiz-Shop/static/js/2.adb8b5cf.chunk.js"
+  },
+  {
+    "revision": "5269950dc1049a3d14874f2abc5793cd",
+    "url": "/Quiz-Shop/static/js/2.adb8b5cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab02da4db701cc46c55f",
+    "url": "/Quiz-Shop/static/js/main.ce5bfe8a.chunk.js"
+  },
+  {
+    "revision": "d2c4744e85a05fa6bdbb",
+    "url": "/Quiz-Shop/static/js/runtime-main.5576d506.js"
+  },
+  {
+    "revision": "1d57cc6a8fc4a0825fe4e38955ad5c80",
+    "url": "/Quiz-Shop/static/media/404face.1d57cc6a.png"
+  },
+  {
+    "revision": "54d18f8ca261bed567b8058e509ea7ab",
+    "url": "/Quiz-Shop/static/media/about-us-background2.54d18f8c.jpg"
+  },
+  {
+    "revision": "b4084e44bea589ec3800fdfdd69cf189",
+    "url": "/Quiz-Shop/static/media/icon-check.b4084e44.svg"
+  },
+  {
+    "revision": "feed2949edb8ce641b847edacfd42a11",
+    "url": "/Quiz-Shop/static/media/quizshop.feed2949.png"
+  },
+  {
+    "revision": "6c3923ea1d954a397ba3fdb46eaf93e2",
+    "url": "/Quiz-Shop/static/media/suggestions-background.6c3923ea.jpg"
+  }
+]);
